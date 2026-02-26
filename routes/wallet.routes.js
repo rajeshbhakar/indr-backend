8bccabbac117b2f07712ac8c94c7a5bb3da4eb8c
rajeshbhakar <rajeshhbhakarr1217@gmail.com>
2026-02-26 09:43:00 +0530

    wallet fixed

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-
 const { protect } = require("../middleware/auth.middleware");
+const { getWallet, addCoins, deductCoins } = require("../controllers/wallet.controller");
 
-router.get("/balance", protect, (req, res) => {
-    
-});
+router.get("/balance", protect, getWallet);
+router.post("/add", protect, addCoins);
+router.post("/deduct", protect, deductCoins);
 
-module.exports = router;
+module.exports = router;    
